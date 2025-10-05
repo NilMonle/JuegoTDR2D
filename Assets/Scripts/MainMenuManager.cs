886fce861720c5b520fddaa22b9b12002bdcd8a6
NilMonle <nilmg2008@gmail.com>
2025-10-05 19:27:10 +0200

    Creacion de MainMenu/OptionsMenu + botones
    05/10
    
    He creado el MainMenu i el OptionsMenu, configurado botones y ya esta todo listo para poder empezar el juego

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+    void Start()
+    {
+        // Asegurar que al inicio el MainMenu esté activo y OptionsMenu oculto
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    public void OpenOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void BackToMain()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!"); // En el editor no se cerrará, solo en el build
+        Application.Quit();
+    }
+    public void StartGame(string Game)
+{
+    SceneManager.LoadScene(Game);
+}
+}
